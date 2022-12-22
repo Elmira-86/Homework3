7b8ad0ab8a43f67388e4f9720568380353b94a52
Elmira-86 <120679545+Elmira-86@users.noreply.github.com>
2022-12-22 19:19:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿//Домашнее задание 1
+
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+
+//12821 -> да
+
+//23432 -> да
+
+// Console.WriteLine("Введите число: ");
+// string number = Console.ReadLine();
+// int pol = number.Length;
+
+// if (pol == 5)
+// {
+//     if (number[0] == number[4] && number[1] == number[3])
+//     {
+//         Console.WriteLine($"{number} - Палиндром");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{number} - Не палиндром");
+//     }
+// }
+
+// Домашнее задание 2
+
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// int x1 = ReadInt("Введите координату X первой точки: ");
+// int y1 = ReadInt("Введите координату Y первой точки: ");
+// int z1 = ReadInt("Введите координату Z первой точки: ");
+// int x2 = ReadInt("Введите координату X второй точки: ");
+// int y2 = ReadInt("Введите координату Y второй точки: ");
+// int z2 = ReadInt("Введите координату Z второй точки: ");
+
+// int A = x2 - x1;
+// int B = y2 - y1;
+// int C = z1 - z2;
+
+// double length = Math.Sqrt(A * A + B * B + C * C);
+// Console.WriteLine($"Длинна отрезка {length}");
+
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// Домашнее задание 3
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// void Main(int Num)
+//  {
+//     int i = 1;
+//       while (i <= Num)
+//       {
+//         Console.WriteLine($"Таблица кубов чисел - {(Math.Pow(i, 3))}");
+//         i++;
+//       }
+//  }
+
+// Console.WriteLine("Введите число");
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// Main(x);
